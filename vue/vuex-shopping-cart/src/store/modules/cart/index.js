89e5a644e589c6068e82c964b750de1aa67c2839
Shubham Tiwari <43375767+tshubham109@users.noreply.github.com>
2021-05-25 18:32:50 +0530

    Add files via upload

---
+++ vue/vuex-shopping-cart/src/store/modules/cart/index.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+const state = {
+  cartItems: [],
+  cartProducts:[]
+}
+const mutations = {
+    UPDATE_CART_ITEMS (state, payload) {
+      state.cartItems = payload;
+    },
+    addNewCartItem(state,newItem){
+      state.cartItems.push(newItem);
+    },
+    UPDATE_CART_PRODUCTS(state,cartProducts){
+     
+      state.cartProducts=cartProducts;
+      console.log(state.cartProducts[0].id)
+    }
+  }
+
+  const actions = {
+    getCartItems ({ commit }) {
+      axios.get('/api/allcartitems').then((response) => {
+        commit('UPDATE_CART_ITEMS', response.data)
+      });
+    },
+    addCartItem ({ commit }, cartItem) {
+     
+        commit('addNewCartItem',cartItem)
+
+    },
+    doCheckOut({ commit,state },user) {
+      console.log(state.user);
+      console.log(user)
+      axios.post('/api/addtocart', ([user,state.cartItems])).then((response) => {
+        commit('UPDATE_CART_ITEMS', [])
+      });
+    },
+    removeCartItem ({ commit }, cartItem) {
+      axios.delete('/api/cart/delete', cartItem).then((response) => {
+        commit('UPDATE_CART_ITEMS', response.data)
+      });
+    },
+    removeAllCartItems ({ commit }) {
+      axios.delete('/api/deletecarts').then((response) => {
+        commit('UPDATE_CART_ITEMS', response.data)
+      });
+    },
+    getCarts({ commit }){
+      axios.get('/api/allcartitems').then((response)=>{
+        console.log(response.data)
+        commit('UPDATE_CART_PRODUCTS',response.data);
+      })
+    }
+  }
+  const getters = {
+    cartProducts:state=>state.cartProducts,
+    cartItems: state => state.cartItems,
+    cartTotal: state => {
+      return state.cartItems.reduce((acc, cartItem) => {
+        return (cartItem.quantity * cartItem.price) + acc;
+      }, 0).toFixed(2);
+    },
+    cartQuantity: state => {
+      return state.cartItems.reduce((acc, cartItem) => {
+        return cartItem.quantity + acc;
+      }, 0);
+    }
+  }
+
+  const cartModule = {
+    state,
+    mutations,
+    actions,
+    getters
+  }
+  export default cartModule;
---
+++ vue/vuex-shopping-cart/src/views/CartProducts.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <span>{{cartProduct.id}}</span>
+       
+        <span v-for="product in cartProduct.products" :key="product.id">
+            
+                {{product.title}},
+                {{product.description}}
+           
+        </span>
+      
+    </div>
+</template>
+
+<script>
+export default{
+    name:'CartProduct',
+    props:["cartProduct"]
+
+}
+</script>
